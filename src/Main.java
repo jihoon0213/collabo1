06b75098b892b9560f1d551e4d1f839ab83f6ba2
jihoon0213 <jihun8138@gmail.com>
2025-07-21 09:49:16 +0900

    팀장 커밋

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("필요없는 파일 지우기");
         System.out.println("기타 필요한 설정 파일 작성하기 ");
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
