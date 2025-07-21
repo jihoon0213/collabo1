8015326167bec24b57a2f0ee8395ed4e41e911d7
jihoon0213 <jihun8138@gmail.com>
2025-07-21 09:32:25 +0900

    두번째 커밋

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("필요없는 파일 지우기");
+        System.out.println("기타 필요한 설정 파일 작성하기 ");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
